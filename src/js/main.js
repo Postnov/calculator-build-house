63fd44726f9e11a23c50360859596991f773b654
Postnov Daniil <dan.postnov@gmail.com>
2018-09-11 10:29:11 +0300

    add calc code

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,64 @@
     Javascript files including like this:
 */
 
-//import ('partails/test.js');
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.querySelector('.calc'),
+        btnCount = document.querySelector('.js-calc-count'),
+        btnReset = document.querySelector('.js-calc-reset'),
+        resultItem = document.querySelector('.calc__result span'),
+        fieldLength = document.querySelector('input[data-calc="length"]'),
+        fieldWidth = document.querySelector('input[data-calc="width"]');
+
+
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+    });
+
+    btnCount.addEventListener('click', function (e) {
+
+        var costItems = document.querySelectorAll('input'),
+            floors = document.querySelectorAll('input[data-floor]'),
+            rectangular = +fieldLength.value * +fieldWidth.value,
+            floorValue,
+            total = 0,
+            basePrice = 300;
+
+        floors.forEach(function (item, i) {
+            if (item.checked) {
+                floorValue = +item.getAttribute('data-floor');
+            }
+        });
+
+        costItems.forEach(function (item, i) {
+            if (item.checked) {
+                var additionCost = +item.getAttribute('data-cost');
+                basePrice += additionCost;
+            }
+        });
+
+        if (isNaN(floorValue)) floorValue = 1;
+
+        basePrice *= floorValue;
+        total = Number(basePrice * rectangular);
+
+        resultItem.innerHTML = total.toLocaleString("ru-RU", {
+            style: "currency",
+            currency: "RUB"
+        });
+    });
+
+    btnReset.addEventListener('click', function () {
+        var costItems = document.querySelectorAll('input');
+
+        costItems.forEach(function (item, i) {
+            item.checked = false;
+            item.value = '';
+        });
+
+        resultItem.innerHTML = '0';
+    });
+
+
+
+});
